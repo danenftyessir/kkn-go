4fd15e91d7175ca971526ce28c91aee0eb770f89
Danendra Shafi Athallah <164164794+danenftyessir@users.noreply.github.com>
2025-10-05 00:50:53 +0700

    add config to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/auth.css', 'resources/css/auth-student.css', 'resources/css/auth-institution.css','resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/css/auth.css', 
+                'resources/css/auth-student.css', 
+                'resources/css/auth-institution.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    base: process.env.APP_ENV === 'production' 
+        ? 'https://kkn-go.replit.app/' 
+        : '/',
 });
