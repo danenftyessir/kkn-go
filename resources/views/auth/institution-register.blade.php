eed0da53cbfe668a38c390ca91280c7c57a52094
Danendra Shafi Athallah <164164794+danenftyessir@users.noreply.github.com>
2025-10-27 23:10:29 +0700

    fix: remove limit announcement

--- resources/views/auth/institution-register.blade.php
+++ resources/views/auth/institution-register.blade.php
@@ -393,16 +393,6 @@
                                         Provinsi <span class="text-red-500">*</span>
                                     </label>
                                     
-                                    {{-- peringatan pembatasan wilayah --}}
-                                    <div class="mb-3 bg-blue-50 border border-blue-200 rounded-lg p-3 flex items-start gap-2">
-                                        <svg class="w-5 h-5 text-blue-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
-                                        </svg>
-                                        <p class="text-sm text-blue-800">
-                                            <span class="font-semibold">Informasi:</span> Saat ini layanan kami hanya tersedia untuk wilayah Jawa (Banten, DKI Jakarta, Jawa Barat, Jawa Tengah, dan DI Yogyakarta).
-                                        </p>
-                                    </div>
-                                    
                                     <select id="province_id" 
                                             name="province_id"
                                             x-model="selectedProvince"
